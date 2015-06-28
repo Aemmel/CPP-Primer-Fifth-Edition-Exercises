d35593a88256fdfb75fbc9615c41fd8eaa28615b
Aemmel <emilkasper@web.de>
2015-06-28 19:00:14 +0200

    Update Exercise_5-17.cpp

--- Exercise_5-17.cpp
+++ Exercise_5-17.cpp
@@ -1,3 +1,40 @@
 // My solution for C++ Primer Fifth Edition, Exercise 5.4.1 from page 185
 //
 
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+	std::vector<int> v = { 0, 1, 1, 2 };
+	std::vector<int> c = { 0, 1, 1, 2, 3, 5, 8 };
+	bool pref = false;
+
+	if (v < c) {
+		auto itv = v.cbegin();
+		auto itc = c.cbegin();
+		
+		while (itv != v.end()) { //only execute until end of 'v', because 'c' cant be smaller than 'v'
+			if (*itv != *itc){
+				break;
+			}
+
+			++itv;
+			++itc;
+		}
+
+		if (itv == v.end()) {
+			pref = true;
+		}
+	}
+
+	if (!pref) {
+		std::cout << "False\n";
+	}
+	else {
+		std::cout << "True\n";
+	}
+
+	return 0;
+}
